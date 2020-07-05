beaaecc46e9fcf3390ffeafd5f311aab817520e0
Habib Kanbar <67437103+habibdevsy@users.noreply.github.com>
2020-07-05 22:06:16 +0300

    Add files via upload

---
+++ src/Controller/Api_name.php
@@ -0,0 +1,78 @@
+<?php
+namespace App\Controller;
+use App\Entity\Name;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\NameRepository;
+use Symfony\Component\HttpFoundation\Request;
+//use Symfony\Component\HttpFoundation\JsonResponse;
+/**
+ * Class Api_name
+ *
+ *
+ * @Route(path="/api/")
+ */
+class Api_name extends AbstractController
+{
+
+    /**
+     * 
+     * @Route("add", name="api_add_name", methods={"POST"})
+     */
+    public function add(Request $request)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        
+        $newName = new Name();
+        
+        $f_name      = $request->request->get("f_name");
+        $l_name      = $request->request->get("l_name"); 
+        $description = $request->request->get("description");
+
+       
+        $errors=[];
+
+        if(empty($f_name) | empty($l_name)){
+           
+            $errors[]="first name or last name are empty";
+            
+            }
+        
+        if (count($errors) > 0) {
+            return $this->json(['errors' => $errors], 400);
+            }
+           
+        $newName->setFName($f_name);
+        $newName->setLName($l_name);
+        $newName->setdescription($description);
+
+        $entityManager->persist($newName);
+        $entityManager->flush();
+            
+       return new Response('Saved new name...');
+       
+        
+    }
+    /**
+     * @Route("list", name="list_name", methods={"GET"})
+     */
+
+    public function list(NameRepository $NameRepository)
+    {
+        $names = $NameRepository->getAll();
+
+            if(!$names){
+                throw $this->createNotFoundException(
+                'Not found the names.. '
+            );
+        }
+        
+         return $this->json(['name'=>$names]);
+        
+     }
+ 
+
+
+}
---
+++ src/Entity/Name.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NameRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=NameRepository::class)
+ */
+class Name
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $f_name;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $l_name;
+
+    /**
+     * @ORM\Column(type="string", length=200, nullable=true)
+     */
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFName(): ?string
+    {
+        return $this->f_name;
+    }
+
+    public function setFName(string $f_name): self
+    {
+        $this->f_name = $f_name;
+
+        return $this;
+    }
+
+    public function getLName(): ?string
+    {
+        return $this->l_name;
+    }
+
+    public function setLName(string $l_name): self
+    {
+        $this->l_name = $l_name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
